71f961a3c2cda7a066b717a8e9989d905cad69b9
Jeremy Gray <jeremygray@utexas.edu>
2019-11-18 04:26:40 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,13 @@
-# require modules here
+require "yaml"
+require 'pp'
 
-def load_library
-  # code goes here
+
+def load_library(file)
+  library=YAML.load_file(file)
+  {:get_meaning=>{},
+    :get_emoticon=>{}
+  }
+  return library
 end
 
 def get_japanese_emoticon
